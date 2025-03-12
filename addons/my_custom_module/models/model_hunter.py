034b413d40f834d7fffbf9a6822ef0f823325ec4
Ognjen <ognjensavic65@gmail.com>
2025-03-12 17:28:43 +0100

    Adding the path to my custom module and adding structure for the hunter modell

--- addons/my_custom_module/models/model_hunter.py
+++ addons/my_custom_module/models/model_hunter.py
@@ -5,5 +5,35 @@ class ModelHunter(models.Model):
     _name = 'my_module.model_hunter'
     _description = 'Model Hunter'
 
-    name = fields.Char(string="Name", required=True)
-    description = fields.Text(string="Description")
+    # Basic personal info
+    name = fields.Char(string="Ime i Prezime", required=True)
+    jmb = fields.Char(string="JMB")  # Jedinstveni Matični Broj
+    birth_year = fields.Integer(string="Godina rođenja")
+    place_of_residence = fields.Char(string="Mjesto stanovanja")
+    address = fields.Char(string="Adresa stanovanja")
+    id_card_number = fields.Char(string="Broj lične karte")
+
+    # Hunter membership info
+    hunter_course_date = fields.Date(string="Datum polaganja lovačke obuke")
+    certificate_number = fields.Char(string="Broj uvjerenja")
+    membership_card_number = fields.Char(string="Broj članske knjižice")
+    membership_date = fields.Date(string="Datum upisa")
+
+    # Weapon info (example with 2 or 3 weapons)
+    weapon1_make = fields.Char(string="Marka 1")
+    weapon1_caliber = fields.Char(string="Kalibar 1")
+    weapon1_serial = fields.Char(string="Serijski broj 1")
+
+    weapon2_make = fields.Char(string="Marka 2")
+    weapon2_caliber = fields.Char(string="Kalibar 2")
+    weapon2_serial = fields.Char(string="Serijski broj 2")
+
+    # Optionally add a 3rd if needed
+    weapon3_make = fields.Char(string="Marka 3")
+    weapon3_caliber = fields.Char(string="Kalibar 3")
+    weapon3_serial = fields.Char(string="Serijski broj 3")
+
+    # Additional fields
+    # e.g., pol, datum rođenja, etc., if the form requires
+    # gender = fields.Selection([('m', 'Muški'), ('f', 'Ženski')], string="Pol")
+    # birth_date = fields.Date(string="Datum rođenja")
